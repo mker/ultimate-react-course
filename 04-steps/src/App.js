a801303002f2ea5c97ae0a0aabfffb320921bb5a
mker <krmichael25@gmail.com>
2025-04-25 15:45:32 +0600

    Add state and logic to switch between steps

--- 04-steps/src/App.js
+++ 04-steps/src/App.js
@@ -1,14 +1,24 @@
+import { useState } from "react";
+
 const messages = ["Learn React ⚛️", "Apply for jobs 💼", "Invest your new income 🤑"];
 
 export default function App() {
-  const step = 1;
+  const [step, setStep] = useState(1);
+
+  function handlePrevious() {
+    if (step > 1) setStep(step - 1);
+  }
+
+  function handleNext() {
+    if (step < 3) setStep(step + 1);
+  }
 
   return (
     <div className="steps">
       <div className="numbers">
-        <div className={`${step >= 1 ? "active" : ""}`}>1</div>
-        <div className={`${step >= 2 ? "active" : ""}`}>2</div>
-        <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+        <div className={step >= 1 ? "active" : ""}>1</div>
+        <div className={step >= 2 ? "active" : ""}>2</div>
+        <div className={step >= 3 ? "active" : ""}>3</div>
       </div>
 
       <p className="message">
@@ -16,8 +26,12 @@ export default function App() {
       </p>
 
       <div className="buttons">
-        <button style={{ backgroundColor: "#7950F2", color: "#fff" }}>Previous</button>
-        <button style={{ backgroundColor: "#7950F2", color: "#fff" }}>Next</button>
+        <button style={{ backgroundColor: "#7950F2", color: "#fff" }} onClick={handlePrevious}>
+          Previous
+        </button>
+        <button style={{ backgroundColor: "#7950F2", color: "#fff" }} onClick={handleNext}>
+          Next
+        </button>
       </div>
     </div>
   );
