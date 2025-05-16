fc134e73ce5bd45a8958f5d39a951ff43d898387
mker <krmichael25@gmail.com>
2025-05-16 19:08:50 +0600

    Revert explicit props in Box component

--- 10-use-popcorn/src/App.jsx
+++ 10-use-popcorn/src/App.jsx
@@ -61,23 +61,13 @@ export default function App() {
         <NumResults movies={movies} />
       </NavBar>
       <Main>
-        <Box element={<MovieList movies={movies} />} />
-        <Box
-          element={
-            <>
-              <WatchedSummary watched={watched} />
-              <WatchedMovieList watched={watched} />
-            </>
-          }
-        />
-
-        {/* <Box>
+        <Box>
           <MovieList movies={movies} />
         </Box>
         <Box>
           <WatchedSummary watched={watched} />
           <WatchedMovieList watched={watched} />
-        </Box> */}
+        </Box>
       </Main>
     </>
   );
@@ -127,7 +117,7 @@ function Main({ children }) {
   return <main className="main">{children}</main>;
 }
 
-function Box({ element }) {
+function Box({ children }) {
   const [isOpen, setIsOpen] = useState(true);
 
   return (
@@ -136,7 +126,7 @@ function Box({ element }) {
         {isOpen ? "–" : "+"}
       </button>
 
-      {isOpen && element}
+      {isOpen && children}
     </div>
   );
 }
